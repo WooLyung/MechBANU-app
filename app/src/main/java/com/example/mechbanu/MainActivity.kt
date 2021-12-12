dc518fe36572a9c30beb1ffa6f025eea003da70f
WooLyung <vlfkdydtk@naver.com>
2021-12-12 20:15:27 +0900

    edit activity layout

--- app/src/main/java/com/example/mechbanu/MainActivity.kt
+++ app/src/main/java/com/example/mechbanu/MainActivity.kt
@@ -1,26 +1,10 @@
 package com.example.mechbanu
 
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
-import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.lang.Exception
-import java.util.*
-import android.os.SystemClock
-
-import android.bluetooth.BluetoothSocket
-import android.os.Build
-import android.util.Log
+import android.widget.ImageButton
 import com.example.mechbanu.bluetooth.Bluetooth
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
-
 
 class MainActivity : AppCompatActivity() {
     var bluetooth: Bluetooth? = null
@@ -29,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button)
-        val textView: TextView = findViewById(R.id.text)
+        val button: ImageButton = findViewById(R.id.microphone)
+        button?.setColorFilter(Color.WHITE)
 
         bluetooth = Bluetooth(this)
 
